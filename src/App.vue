06df751ffee79ccbd2cf56302ed087c15fa6cc4c
Rangga Adithia <rangga.adithia26@gmail.com>
2023-10-04 21:22:25 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="px-4 md:px-40">
+  <div id="app">
+      <AddPostForm @add-post="addPostToArray" />
+      <ul>
+        <li v-for="post in posts" :key="post.id" class="w-full flex flex-col md:flex-row mt-10 bg-slate-100 rounded-xl overflow-hidden">
+          <img src="https://source.unsplash.com/300x200/?technology" alt="" class="basis-1/4">
+          <div class="ml-10 py-3">
+            <h2 class="text-4xl font-semibold">{{ post.title }}</h2>
+            <p class="mt-1">{{ post.content }}</p>
+          </div>
+        </li>
+      </ul>
+  </div>
+  </div>
+</template>
+
+<script>
+import AddPostForm from "./components/AddPost.vue";
+
+export default {
+ components: { 
+    AddPostForm,
+ },
+ data() {
+    return {
+      posts: [],
+    };
+ },
+ methods: {
+    addPostToArray(post) {
+      this.posts.push(post);
+    },
+ },
+};
+</script>
---
+++ src/components/AddPost.vue
@@ -0,0 +1,43 @@
+<template>
+ <div>
+    <h1 class="text-center text-4xl font-semibold mt-10">Add Post</h1>
+    <form @submit.prevent="addPost">
+      <div>
+        <label for="title" class="font-semibold">Title:</label>
+        <input type="text" id="title" v-model="title" required class="ring-2 mb-3 w-full py-2 rounded-md px-2"/>
+      </div> 
+      <div>
+        <label for="content" class="font-semibold">Content:</label>
+        <textarea id="content" v-model="content" required class="ring-2 w-full rounded-md px-2 py-1" rows="4"></textarea>
+
+      </div>
+      <div class="w-full flex justify-center mt-3">
+
+        <button type="submit" class="px-4 py-3 bg-sky-900 text-white rounded-2xl font-bold w-full md:w-fit ">Add Post</button>
+      </div>
+    </form>
+ </div>
+</template>
+
+<script>
+export default {
+ data() {
+    return {
+      title: "",
+      content: "",
+    };
+ },
+ methods: {
+    addPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        content: this.content,
+      };
+      this.$emit("add-post", newPost);
+      this.title = "";
+      this.content = "";
+    },
+ },
+};
+</script>
